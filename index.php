1b2caa1fbeafd4fd8f3bbb54f6fa601ec19fc2ac
BenArief <benarief29@gmail.com>
2025-01-29 16:23:02 +0700

    Menambahkan div class satu dan css di index.php

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coba git remote 2</title>
+    <style>
+        .satu {
+            background-color: salmon;
+            margin: 50px auto;
+        }
+    </style>
 </head>
+
 <body>
     <?php
-        echo "Hello World";
+    echo "Hello World";
     ?>
 
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam, maxime.</p>
+    <div class="satu">Miaw</div>
 </body>
+
 </html>
